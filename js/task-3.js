bcf21d28e4e5ae1610eb5007dfb8a54e37f371c5
Olga Blud <olga.blud@gmail.com>
2024-04-15 13:36:45 +0300

    task 3 done

--- js/task-3.js
+++ js/task-3.js
@@ -1,30 +1,22 @@
-// const profile = {
-//   username: 'Jacob',
-//   playTime: 300,
-// };
-// console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+console.log('Задача 3. Профіль гравця');
+const profile = {
+  username: 'Jacob',
+  playTime: 300,
+  changeUsername(newName) {
+    this.username = newName;
+  },
+  updatePlayTime(hours) {
+    this.playTime = this.playTime + hours;
+  },
+  getInfo() {
+    const message = `${this.username} has ${this.playTime} active hours!`;
+    return message;
+  },
+};
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
 
-// profile.changeUsername('Marco');
-// console.log(profile.getInfo()); // "Marco has 300 active hours!"
+profile.changeUsername('Marco');
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
 
-// profile.updatePlayTime(20);
-// console.log(profile.getInfo()); // "Marco has 320 active hours!"
-
-/* 
-Задача 3. Профіль гравця
-
-Об’єкт profile описує профіль користувача на ігровій платформі. У його
-властивостях зберігається ім’я профілю username та кількість активних годин
-playTime, проведених у грі.
-
-const profile = { username: "Jacob", playTime: 300, };
-
-Доповни об’єкт profile методами для роботи з його властивостями.
-
-Метод changeUsername(newName) повинен приймати рядок (нове ім’я) в параметр
-newName та змінювати значення властивості username на нове. Нічого не повертає.
-Метод updatePlayTime(hours) повинен приймати число (кількість годин) у параметр
-hours та збільшити на нього значення властивості playTime. Нічого не повертає.
-Метод getInfo() має повертати рядок формату <Username> has <amount> active
-hours!, де <Username> — це ім’я профілю, а <amount> — кількість ігрових годин.
- */
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
